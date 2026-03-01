468fde96ddab411e1b626bd6a148c2c3b82a8ec1
drferozshaikh <drferozshaikh@yahoo.com>
2026-03-01 16:41:10 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+const btn = document.getElementById("guidanceBtn");
+const menu = document.querySelector(".dropdown-menu");
+
+btn.addEventListener("click", function(e){
+e.preventDefault();
+menu.classList.toggle("show");
+});
+
+/* close when clicking outside */
+
+document.addEventListener("click", function(e){
+if (!btn.contains(e.target) && !menu.contains(e.target)){
+menu.classList.remove("show");
+}
+});
+
+});
